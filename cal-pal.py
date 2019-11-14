91086c3b2bcd186b044cc7979f8e47522412af32
Jaber Alshehri <56383540+jaber-tech@users.noreply.github.com>
2019-11-14 08:34:38 +0300

    Create cal-pal.py

---
+++ cal-pal.py
@@ -0,0 +1,11 @@
+# This program is simple calculator for VAT in Python 
+num=0
+numvat
+i=""
+
+num=input ("enter the price before VAT: ")
+numvat = num + (num*0.05)
+print ("Total price with VAT is : " + str(numvat))
+print ("desgined bu jab-public")
+
+
